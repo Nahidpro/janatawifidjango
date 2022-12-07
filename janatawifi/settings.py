c873ae111f36e1393158389aa8496f246c3d5095
Nahidpro <37673505+Nahidpro@users.noreply.github.com>
2022-12-07 23:50:52 +0600

    Update settings.py

--- janatawifi/settings.py
+++ janatawifi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--uz9#%5q7z=5a@&&zl3l^*52-ijhv=nik-991@7n!3(&)*bc*%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
